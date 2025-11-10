6348f85f65ef51c10e57f2b8a2eda72ef275cecf
AkashAIML13 <aksandimath@gmail.com>
2025-11-10 16:40:03 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,14 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+addition=a+b
+substraction=a-b
+multiplication=a*b
+div=a/b
+div1=a//b
+div2=a%b
+print(addition,substraction,multiplication)
+print(div,div1,div2)
+enter first number5
+enter second number5
+10 0 25
+1.0 1 0
